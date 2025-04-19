d44701116f0231e6785c0aa6f406b770a08b4798
rdmjlk <119345299+rdmjlk@users.noreply.github.com>
2025-04-19 14:19:08 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255,255,255,0);
-  stroke(255);
+  stroke(0);
   strokeWeight(0.5);
   frameRate(8);
   blendMode(DIFFERENCE);
