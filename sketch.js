e9e182f46ccba01b90c2f1aa53def2502d25f07a
rdmjlk <119345299+rdmjlk@users.noreply.github.com>
2025-04-19 16:42:57 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var canvas;
 function setup() {
   canvas= createCanvas(windowWidth, windowHeight);
   canvas.position(0,0);
-  canvas.style('z-index','-999');
+  canvas.style('z-index','-9');
   stroke(0);
   strokeWeight(0.5);
   frameRate(8);
