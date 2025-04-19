bbe5112bdc176880fd745d19826a814216b2646d
rdmjlk <119345299+rdmjlk@users.noreply.github.com>
2025-04-19 15:14:14 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
+var canvas;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  canvas= createCanvas(windowWidth, windowHeight);
+  canvas.position(0,0);
+  canvas.style('z-index','-1');
   stroke(0);
   strokeWeight(0.5);
   frameRate(8);
